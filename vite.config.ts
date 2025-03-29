091070ffd1ecd39932fbd14b63a672b24ee59432
unknown <markmark092100@gmail.com>
2025-03-30 03:21:48 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/testss/',
+  base: '/animon/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
